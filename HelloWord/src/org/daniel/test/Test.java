af14b63afdaaa10f130866c54f024a0f8d8a3e31
Daniel Deng <Daniel.ninghua@gmail.com>
2013-08-24 00:27:17 +0800

    Attached to Git

---
+++ HelloWord/src/org/daniel/test/Test.java
@@ -0,0 +1,28 @@
+package org.daniel.test;
+
+public class Test
+{
+
+    public static void main(String[] args)
+    {
+        StringBuilder msg = new StringBuilder();
+        
+        msg.append("a").append(",").append("b").append(",").append("c").append(",");
+        System.out.println(msg.toString());
+        
+        msg.deleteCharAt(msg.length() -1);
+        System.out.println(msg.toString());
+        
+        try
+        {
+            throw new IllegalArgumentException("IllegalArgumentException for example");
+        }
+        catch(Exception e)
+        {
+            
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
